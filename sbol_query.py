82e299bf8215b93ea38de6779eb065ab9b1e25ee
Jeffrey David Johnson <jefdaj@gmail.com>
2012-05-08 11:14:57 -0700

    cleaned up sbol_query; now it just needs some more queries

--- sbol_query.py
+++ sbol_query.py
@@ -1,8 +1,6 @@
-__all__ = (
-    'RDF', 'RDFS', 'REGISTRY', 'SBOL',
-    'Variable', 'BNode', 'URIRef', 'Operator', 'Literal',
-    'SBOLQuery', 'SBOLResult', 'SBOLNode',
-    'SBPKB2')
+#######################
+# imports and exports
+#######################
 
 import urllib2
 from rdflib import Namespace, Literal, URIRef, BNode, URIRef
@@ -30,13 +28,24 @@ Operator.desc = desc
 del is_a, and_, or_
 del asc, desc
 
-SBOL     = Namespace('http://sbols.org/v1#')
-REGISTRY = Namespace('http://partsregistry.org/#')
+__all__ = (
+    'RDF', 'RDFS', 'SB', 'PR', 'SO',
+    'Variable', 'BNode', 'URIRef', 'Operator', 'Literal',
+    'SBOLQuery', 'SBOLResult', 'SBOLComponent', 'SBOLNode',
+    'SBPKB2',
+    'test')
+
+##########################
+# SPARQL wrapper classes
+##########################
+
+SB = Namespace('http://sbols.org/v1#')
+PR = Namespace('http://partsregistry.org/#')
+SO = Namespace('http://purl.org/obo/owl/SO#')
 
 class SBOLQuery(Select):
 
-    # todo remove keyword
-    def __init__(self, keyword=None, limit=500):
+    def __init__(self):
 
         # create query elements
         # to be processed during compilation
@@ -45,22 +54,24 @@ class SBOLQuery(Select):
         self.OPTIONAL = []
         self.FILTER   = []
         self.DISTINCT = True
-        self.LIMIT    = limit
-        self.ORDER_BY = None # todo order by Id
+        self.LIMIT    = None
+        self.ORDER_BY = None
 
-        # create the result variable
+        # create the result Variable
         # this one is special because it represents
         # the result object itself, rather than one
         # of its attributes
         self.result = Variable('result')
 
-        # set up the default query
-        display_id = Variable('displayId')
-        self.SELECT.append(display_id)
+        # results must be available DnaComponents with displayIds
+        self.WHERE.append((self.result, RDF.type, SB.DnaComponent))
+        self.add_attribute(SB.displayId, 'displayId')
+
+        # although not a requirement, most people
+        # probably want this to be True
+        self.available_only = False
+
         Select.__init__(self, self.SELECT)
-        self.WHERE.append((self.result, SBOL.displayId, display_id))
-        self.WHERE.append((self.result, RDF.type, SBOL.DnaComponent))
-        #self.available_only = True
 
     def compile(self):
         'Builds the query and returns it as a str'
@@ -71,9 +82,8 @@ class SBOLQuery(Select):
                             _order_by=self.ORDER_BY,
                             _distinct=self.DISTINCT)
 
-        # todo put this back once SBPkb2 has sbol:status
-        #if self.available_only:
-        #    query = query.where(( self.result, SBOL.status, Literal('Available') ))
+        if self.available_only:
+            query = query.where(( self.result, SB.status, Literal('Available') ))
 
         # add each stored graph pattern
         for clause in self.WHERE:
@@ -84,7 +94,8 @@ class SBOLQuery(Select):
             query = query.filter(expression)
 
         # compile to a str
-        return Select.compile(query)
+        query = Select.compile(query)
+        return query
 
     def __repr__(self):
         return '<%s %s>' % (self.__class__.__name__, self.__dict__)
@@ -107,9 +118,9 @@ class SBOLQuery(Select):
         '''
         Require each result pattern to have a triple like:
             <?result> <rdf_predicate> <?attr_name>
-        and make <?attr_name> an attribute of the resulting SBOLResult.
+        and make <?attr_name> an attribute of the result.
         For example:
-            query.add_attribute(SBOL.longDescription, 'long')
+            query.add_attribute(SB.longDescription, 'long')
         '''
         var = Variable(attr_name)
         self.SELECT.append(var)
@@ -119,31 +130,40 @@ class SBOLQuery(Select):
             destination = self.WHERE
         destination.append((self.result, rdf_predicate, var))
 
-    # todo change to add_so_type
-    def add_registry_type(self, registry_type):
-        'Adds a WHERE clause specifying the REGISTRY type of each result'
+    def add_type(self, rdf_type):
+        'Adds a WHERE clause specifying the type of each result'
+        self.WHERE.append((self.result, RDF.type, rdf_type))
 
-        # using REGISTRY.registry_type would include 'registry_type'
-        # literally, so the URIRef is constructed manually instead
-        ref = URIRef(REGISTRY + registry_type)
-        self.WHERE.append((self.result, RDF.type, ref))
-
-    # todo add so_type as an optional default attribute
+    # todo add_so_type as an optional default attribute
 
 class SBOLResult(QueryResult):
 
-    def convert(self):
-        json = QueryResult.convert(self)
-        results = []
-        for binding in json['results']['bindings']:
-            result = SBOLResult()
-            for key in binding:
-                result.__setattr__(key, binding[key]['value'])
-            results.append(result)
-        return results
-
     def __repr__(self):
         return '<%s %s>' % (self.__class__.__name__, self.__dict__)
+
+    def convert(self):
+        json = QueryResult.convert(self)
+        components = []
+        try:
+            for binding in json['results']['bindings']:
+                com = SBOLComponent()
+                for key in binding:
+                    value = binding[key]['value']
+                    com[key] = value
+                components.append(com)
+            return components
+        except KeyError:
+            return components
+
+class SBOLComponent(dict):
+
+    def __repr__(self):
+        return '<%s %s>' % (self.__class__.__name__, dict.__repr__(self))
+
+    #def __setitem__(self, key, value):
+    #    key   = key.encode('utf-8')
+    #    value = value.encode('utf-8')
+    #    dict.__setitem__(self, key, value)
 
 class SBOLNode(SPARQLWrapper):
 
@@ -157,14 +177,38 @@ class SBOLNode(SPARQLWrapper):
         return "<%s '%s'>" % (self.__class__.__name__, self.baseURI)
 
     def query(self):
-        return SBOLResult(self._query())
+        try:
+            return SBOLResult(self._query())
+        except urllib2.URLError:
+            message = 'Unable to reach the server'
+            LOG.error(message)
+            print message
+            return []
 
     def execute(self, query):
         # todo remove/rename this?
         self.setQuery(query.compile())
-        return self.query()
+        result = self.query()
+        result = result.convert()
+        return result
+
+####################
+# known SBOL nodes
+####################
 
 SBPKB2 = SBOLNode('http://sbpkb2.sbols.org:8989/sbpkb2/query',
                   username='anonymous',
                   password='anonymous')
+
+####################
+# very simple test
+####################
+
+def test():
+    q = SBOLQuery()
+    q.LIMIT = 10
+    return SBPKB2.execute(q)
+
+if __name__ == '__main__':
+    print test()
 
